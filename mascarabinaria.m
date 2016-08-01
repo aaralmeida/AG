a07fab7c0764a046508f4a12e1833e0da02decf0
Armando <armandoalanalmeida@gmail.com>
2016-08-01 14:19:25 -0300

    Mascara Binaria

---
+++ mascarabinaria.m
@@ -0,0 +1,13 @@
+function popfilhos = mascarabinaria(popPais)
+    mascara = randi([0 1],1,30)
+    for i = 1:2:height(popFilhos)
+        for i = 1:1:height(mascara)
+            if mascara[i] == 0
+                filho1 =
+            else
+            end
+        end
+        filho2=
+    end
+    
+end
